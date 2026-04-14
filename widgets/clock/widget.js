3676b7a7795098b64c9ad0c654770ee4c8cd926b
Paul M. Dorr <paul.mdorr@gmail.com>
2026-04-14 17:44:26 -0300

    improve clock pm/am style

--- widgets/clock/widget.js
+++ widgets/clock/widget.js
@@ -3,10 +3,19 @@ widget.render();
 
 function tick() {
   const now = new Date();
+  let time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  let period = '';
+
+  if (time.match(/\d{2}:\d{2} [P|A]M/g)) {
+    period = time.slice(6);
+    time = time.slice(0, 5);
+  }
+
   widget.setState({
-    time: now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+    time,
     day: now.toLocaleDateString([], { weekday: 'long' }),
     date: now.toLocaleDateString([], { day: 'numeric', month: 'long' }),
+    period,
   });
 }
 
